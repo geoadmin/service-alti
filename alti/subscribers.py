6329679da23c45d508363b4f884198b996c0efdb
loicgasser <loicgasser4@gmail.com>
2017-12-14 11:58:22 +0100

    Reject POST requests without content length

--- alti/subscribers.py
+++ alti/subscribers.py
@@ -1,15 +1,21 @@
 # -*- coding: utf-8 -*-
 
 from pyramid.events import subscriber, NewRequest
-from pyramid.httpexceptions import HTTPRequestEntityTooLarge
+from pyramid.httpexceptions import HTTPBadRequest, HTTPRequestEntityTooLarge
 
 
 @subscriber(NewRequest)
 def validate_body_length(event):
+    content_length = 0
     max_clength = 160000
-    content_length = event.request.headers.get('Content-Length')
-    content_length = content_length or len(event.request.params.get('geom', ''))
-    if content_length and int(content_length) > max_clength:
+    if event.request.method == 'POST':
+        content_length = event.request.headers.get('Content-Length')
+        if not content_length:
+            raise HTTPBadRequest('No Content-Length was provided in request header')  # pragam: no cover
+    elif event.request.method == 'GET':
+        content_length = len(event.request.params.get('geom', ''))
+
+    if int(content_length) > max_clength:
         raise HTTPRequestEntityTooLarge(
             'LineString too large, maximum Content-Length ' +
             'of %s exceeded' % max_clength
