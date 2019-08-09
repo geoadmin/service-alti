75b6797d4f74ca685243c527ecbe5e72347cd716
Pascal Barth <pak.barth@gmail.com>
2019-08-09 15:59:55 +0200

    Changing another test linestring to be located in the available tile

--- alti/tests/integration/test_profile.py
+++ alti/tests/integration/test_profile.py
@@ -266,7 +266,7 @@ class TestProfileView(TestsBase):
         self.assertTrue(resp.content_type == 'application/json')
 
     def test_profile_lv95_nb_points_exceeds_resolution_meshing(self):
-        resp = self.__get_json_profile(params={'geom': '{"type": "LineString", "coordinates": [[2601180.3, 1196948.3], [2601148.4, 1196914.8]]}',
+        resp = self.__get_json_profile(params={'geom': TestProfileView.__LINESTRING_VALID_LV95,
                                                'nb_points': 150},
                                        expected_status=203)
         self.assertTrue(resp.content_type == 'application/json')
