7aee30a25198b1dd33dc2a3ef041af081724c1ce
Pascal Barth <pak.barth@gmail.com>
2019-11-28 07:25:26 +0100

    Accept geom in HTTP POST body (#47)
    * Accept geom in HTTP POST body
    
    * Updating INT infra for branch deploy
    
    * typo
    
    * Checking that geom is present when in body

--- alti/views/profile.py
+++ alti/views/profile.py
@@ -16,7 +16,10 @@ class Profile(ProfileValidation):
         super(Profile, self).__init__()
         self.nb_points_default = int(request.registry.settings.get('profile_nb_points_default', 200))
         self.nb_points_max = int(request.registry.settings.get('profile_nb_points_maximum', 500))
-        self.linestring = request.params.get('geom')
+        if 'geom' in request.params:
+            self.linestring = request.params.get('geom')
+        else:
+            self.linestring = request.body
         if 'layers' in request.params:
             self.layers = request.params.get('layers')
         else:
