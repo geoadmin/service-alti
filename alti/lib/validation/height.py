2bd6b73972e1de57ddea221f0ae0aff263b1b995
Martin Kuenzi <39826573+ltkum@users.noreply.github.com>
2020-02-17 15:14:11 +0100

    I'm bad at finding those, I guess

--- alti/lib/validation/height.py
+++ alti/lib/validation/height.py
@@ -10,7 +10,6 @@ class HeightValidation(object):
     def __init__(self):
         self._lon = None
         self._lat = None
-        self._layers = None
         self._sr = None
 
     @property
@@ -20,10 +19,6 @@ class HeightValidation(object):
     @property
     def lat(self):
         return self._lat
-
-    @property
-    def layers(self):
-        return self._layers
 
     @property
     def sr(self):
@@ -46,15 +41,6 @@ class HeightValidation(object):
             self._lat = float_raise_nan(value)
         except ValueError:
             raise HTTPBadRequest("Please provide numerical values for the parameter 'northing'/'lat'")
-
-    @layers.setter
-    def layers(self, value):
-        if not isinstance(value, list):
-            value = value.split(',')
-            for i in value:
-                if i not in ('DTM25', 'DTM2', 'COMB'):
-                    raise HTTPBadRequest("Please provide a valid name for the elevation model DTM25, DTM2 or COMB")
-        self._layers = value
 
     @sr.setter
     def sr(self, value):
