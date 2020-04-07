4f24aba01e138396788e3a6a9ccb14b9f7be9253
Pascal Barth <pak.barth@gmail.com>
2020-04-07 07:58:04 +0200

    Simplfication of file handling

--- alti/lib/raster/georaster.py
+++ alti/lib/raster/georaster.py
@@ -51,7 +51,6 @@ class BinaryTerrainTile(object):
         self.max_y = max_y
 
         self.filename = filename
-        self.file = None
         self.first_reading = True
 
         self.cols = None
@@ -61,46 +60,33 @@ class BinaryTerrainTile(object):
         self.resolution_x = None
         self.resolution_y = None
 
-    def __del__(self):
-        self.close_file()
-
     def __str__(self):
         return "%f, %f, %f, %f: %s" % (self.min_x, self.min_y, self.max_x, self.max_y, self.filename)
-
-    def __open_file__(self):
-        if self.file is None or self.file.closed:
-            self.file = open(self.filename, 'rb')
-
-    def close_file(self):
-        if self.file is not None and not self.file.closed:
-            self.file.close()
 
     def contains(self, x, y):
         return self.min_x <= x < self.max_x and self.min_y <= y < self.max_y
 
     def get_height_for_coordinate(self, x, y):
-        # opening file
-        self.__open_file__()
-        # Reading file metadata if it's the first time reading it
-        if self.first_reading:
-            self.file.seek(10)
-            (self.cols, self.rows, self.data_size, self.floating_point) = unpack('<LLhh', self.file.read(12))
-            self.resolution_x = (self.max_x - self.min_x) / self.cols
-            self.resolution_y = (self.max_y - self.min_y) / self.rows
-            self.first_reading = False
-        position_x = int((x - self.min_x) / self.resolution_x)
-        position_y = int((y - self.min_y) / self.resolution_y)
-        self.file.seek(256 + (position_y + position_x * self.rows) * self.data_size)
-        if self.floating_point == 1:
-            format_to_unpack = "<f"
-        else:
-            if self.data_size == 2:
-                format_to_unpack = "<h"
+        with open(self.filename, 'rb') as file:
+            # Reading file metadata if it's the first time reading it
+            if self.first_reading:
+                file.seek(10)
+                (self.cols, self.rows, self.data_size, self.floating_point) = unpack('<LLhh', file.read(12))
+                self.resolution_x = (self.max_x - self.min_x) / self.cols
+                self.resolution_y = (self.max_y - self.min_y) / self.rows
+                self.first_reading = False
+            position_x = int((x - self.min_x) / self.resolution_x)
+            position_y = int((y - self.min_y) / self.resolution_y)
+            file.seek(256 + (position_y + position_x * self.rows) * self.data_size)
+            if self.floating_point == 1:
+                format_to_unpack = "<f"
             else:
-                format_to_unpack = "<l"
-        value = unpack(format_to_unpack, self.file.read(self.data_size))[0]
-        self.close_file()
-        return value
+                if self.data_size == 2:
+                    format_to_unpack = "<h"
+                else:
+                    format_to_unpack = "<l"
+            value = unpack(format_to_unpack, file.read(self.data_size))[0]
+            return value
 
 
 class GeoRaster:
